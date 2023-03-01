bed2ead0bfb60a22ca4eb7d424277ac7dafd0168
Erald Guri <44472105+eraldguri@users.noreply.github.com>
2023-03-01 19:44:13 +0100

    project setup

---
+++ app/src/main/java/com/erald_guri/mobileproject/PhotoFragment.kt
@@ -0,0 +1,13 @@
+package com.erald_guri.mobileproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class PhotoFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
